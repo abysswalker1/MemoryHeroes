75c4cefa4afa1abd3ac9037d857038f58ae36309
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-03-15 18:45:23 +0500

    Without REF

--- MemoryHeroes/src/components/App.js
+++ MemoryHeroes/src/components/App.js
@@ -4,77 +4,38 @@ import Card from './cards/Card.js';
 import { cardData } from '../cardData';
 
 export default function App() {
-  const [cards, setCards] = React.useState([]);
-  const openedCardsRef = React.useRef([]);
-
-  const unfoldCards = (func) => {
-    setCards(
-      cardData.map((item) => {
-        return <Card key={item.id} item={item} onClick={func} />;
-      }),
-    );
-  };
-
-  const cardsRefTransform = () => {
-    let compareCardsRef = openedCardsRef.current.reduce((prevCard, card) => {
-      return prevCard.detach === card.detach;
-    });
-
-    let changeCardValue = (num, value) => {
-      cardData.forEach((elem) => {
-        if (value) {
-          if (elem.id === openedCardsRef.current[num].id) 
-            elem.guessed = true;
-        } else {
-          if (elem.id === openedCardsRef.current[num].id) 
-            elem.active = false;
-        }
-      })
-    }
-
-    if (compareCardsRef) {
-      changeCardValue(0, true)
-      changeCardValue(1, true)
-    } else {
-      changeCardValue(0, false)
-      changeCardValue(1, false) 
-    }
-  };
-
-  const addCardToCardsRef = (item) => {
-    if(!item.active && !item.guessed) {
-    openedCardsRef.current.push(item)
-     item.active = !item.active
-    }
-  }
+  const [cards, setCards] = React.useState([])
+  const [openedCards, setOpenedCards] = React.useState([]);
 
   const handleClick = (item) => {
-    if (openedCardsRef.current.length < 1) {
-      addCardToCardsRef(item)
-      unfoldCards(handleClick)
-    } else {
-      addCardToCardsRef(item)
-      unfoldCards(null);
-      setTimeout(
-        (func) => {
-          cardsRefTransform();
-          unfoldCards(func);
-          openedCardsRef.current = [];
-        },
-        1000,
-        handleClick,
-      );
-    }
-  };
+   setOpenedCards((prev) => {
+     return prev.concat(item)
+   })
+  }
 
-  const mixCards = () => {
-    cardData.sort(() => Math.random() - 0.5);
-    unfoldCards(handleClick);
-  };
+  const unfoldCards = (func) => {
+    setCards (
+      cardData.map(card => {
+        return <Card key = {card.id}  item = {card} func = {func} openedCards = {openedCards} />
+      })
+    )
+  }
 
   useEffect(() => {
-    mixCards();
+    if(openedCards.length == 2) {
+      unfoldCards(null)
+      setTimeout((func) => {
+        setOpenedCards([])
+        unfoldCards(func)
+      }, 1000, handleClick)
+    }
+  }, [openedCards])
+
+  useEffect(() => {
+    cardData.sort(() => Math.random() - 0.5);
+    unfoldCards(handleClick)
   }, []);
+
 
   return (
     <div className="app">
--- MemoryHeroes/src/components/cards/Card.js
+++ MemoryHeroes/src/components/cards/Card.js
@@ -1,22 +1,42 @@
-import React from "react";
-import './Card.css'
+import React, { useEffect } from 'react';
+import './Card.css';
 
 export default function Card(props) {
+  const [active, setActive] = React.useState(props.item.guessed);
 
-    const handleClick = () => {
-        if(props.onClick && !props.item.active) {
-            props.onClick(props.item)
-        } 
+  const handleClick = () => {
+    if (!active && props.func) {
+      setActive(true);
+      props.func(props.item);
     }
+  };
 
-    const isActiveOrGuessed = props.item.active || props.item.guessed
+  const checkDetach = () => {
+    if (props.item.id === props.openedCards[0].id || props.item.id === props.openedCards[1].id) {
+      props.item.guessed = true;
+    }
+  };
 
-    return (
-        <div className="card" onClick ={() => handleClick()}>
-            <div className="card-front" style = {isActiveOrGuessed ? {transform : 'rotateY(180deg)'} : null}></div>
-            <div className="card-back" style = {isActiveOrGuessed ? {transform : 'rotateY(360deg)'} : null}>
-                <img className="card-back-image" src = {`${props.item.image}`}/>
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    if (props.openedCards.length == 2 && !props.item.guessed) {
+      const compareTwoCards = props.openedCards.reduce((first, second) => {
+        return first.detach === second.detach;
+      });
+
+      setTimeout(() => {
+        if (compareTwoCards) {
+          checkDetach();
+        } else setActive(false);
+      }, 1000);
+    }
+  }, [props.openedCards]);
+
+  return (
+    <div className="card" onClick={() => handleClick()}>
+      <div className="card-front" style={active ? { transform: 'rotateY(180deg)' } : null}></div>
+      <div className="card-back" style={active ? { transform: 'rotateY(360deg)' } : null}>
+        <img className="card-back-image" src={`${props.item.image}`} />
+      </div>
+    </div>
+  );
 }
